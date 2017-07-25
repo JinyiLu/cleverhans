1464a531d3ebb818e8e008b1a51080383fb43c8e
Jinyi Lu <jinyil@jinyil-m01.vmware.com>
2017-07-25 15:35:32 -0700

    start with lr

---
+++ examples/nips17_adversarial_competition/dev_defenses/experimental_model/train.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import numpy as np
+from scipy.misc import imread
+
+import tensorflow as tf
+
