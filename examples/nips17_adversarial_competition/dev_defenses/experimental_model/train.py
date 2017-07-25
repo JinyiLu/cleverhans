85e02299c0407acdd94380a4f0c94d96b958c8a0
Jinyi Lu <jinyil@jinyil-m01.vmware.com>
2017-07-25 16:08:12 -0700

    exp

--- examples/nips17_adversarial_competition/dev_defenses/experimental_model/train.py
+++ examples/nips17_adversarial_competition/dev_defenses/experimental_model/train.py
@@ -9,3 +9,26 @@ from scipy.misc import imread
 
 import tensorflow as tf
 
+tf.flags.DEFINE_string(
+    'master', '', 'The address of the TensorFlow master to use.')
+
+tf.flags.DEFINE_string(
+    'checkpoint_path', '', 'Path to checkpoint for inception network.')
+
+tf.flags.DEFINE_string(
+    'input_dir', '', 'Input directory with images.')
+
+tf.flags.DEFINE_string(
+    'output_file', '', 'Output file to save labels.')
+
+tf.flags.DEFINE_integer(
+    'image_width', 299, 'Width of each input images.')
+
+tf.flags.DEFINE_integer(
+    'image_height', 299, 'Height of each input images.')
+
+tf.flags.DEFINE_integer(
+    'batch_size', 16, 'How many images process at one time.')
+
+FLAGS = tf.flags.FLAGS
+
